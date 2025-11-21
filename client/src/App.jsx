3c109aa989fc778bfd33fb7ce4be399d01d01b05
Yosa <yosazefamawuntu@gmail.com>
2025-11-21 14:31:22 +0800

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+
+  return (
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard " />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+          <Route path="/dashboard" element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          } />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  )
+}
+
+export default App
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { Container, Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
+const Dashboard = () => {
+    const { user } = useContext(AuthContext);
+    console.log("USER DARI CONTEXT:", user);
+
+    return(
+        <Container className="mt-5">
+            <h1 className="mb-4">Dashboard</h1>
+            <Card>
+                <Card.Body>
+                    <h3>Selamat Datang, {user?.username}!</h3>
+                    <p><strong>Email:</strong> {user?.email}</p>
+                </Card.Body>
+            </Card>
+        </Container>
+    );
+};
+
+export default Dashboard;
